9b39fb969aa6b4a3b0a7c65988cc07b64bf89bfa
wubh2012 <wubh2012@163.com>
2019-08-12 16:10:09 +0800

    压缩css,根据env环境使用不同的打包配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,83 @@
 const path = require('path');
 const APPDIR = 'src/';
-const devMode = process.env.NODE_ENV !== 'production';
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: path.resolve(__dirname, APPDIR, 'index.html'),
-  filename: 'index.html',
-  inject: 'body'
-});
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = {
-  context: path.resolve(__dirname, APPDIR),
-  mode: 'production', // development, production
-  entry: {
-    index: './js/index.js',
-  },
-  devtool: 'source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [{
-        test: /\.css|\.scss|\.sass$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: devMode === 'development',
-            },
-          },
-          'css-loader',
-          'sass-loader',
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          {
+module.exports = env => {
+
+  console.log('NODE_ENV: ', env.NODE_ENV); // 'dev' or production
+  const devMode = env.NODE_ENV !== 'production';
+
+  return {
+    context: path.resolve(__dirname, APPDIR),
+    entry: {
+      index: './js/index.js',
+    },
+    devtool: 'source-map',
+    devServer: {
+      contentBase: './dist',
+      hot: true
+    },
+    output: {
+      filename: devMode ? '[name].js' : '[name].[hash].js',
+      chunkFilename: devMode ? '[id].js' : '[id].[hash].js',
+      path: path.resolve(__dirname, 'dist'),
+    },    
+    module: {
+      rules: [{
+          test: /\.css|\.scss|\.sass$/,
+          use: [{
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: devMode,
+              },
+            },            
+            'css-loader',
+            'sass-loader',
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [{
             loader: 'file-loader',
             options: {
               outputPath: 'img/'
             }
-          }          
-        ]
-      },
-      {
-        test: /\.(ogg|mp3|wav|mpe)$/,
-        use: [
-          {
+          }]
+        },
+        {
+          test: /\.(ogg|mp3|wav|mpe)$/,
+          use: [{
             loader: 'file-loader',
             options: {
               outputPath: 'songs/'
             }
-          }          
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    HTMLWebpackPluginConfig,
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // all options are optional
-      filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-    }),
-  ],
+          }]
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, APPDIR, 'index.html'),
+        filename: 'index.html',
+        inject: 'body'
+      }),
+      new MiniCssExtractPlugin({        
+        filename: devMode ? '[name].css' : '[name].[hash].css',
+        chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      }),
+      new OptimizeCSSAssetsPlugin({
+        assetNameRegExp: /\.css\.*(?!.*map)/g,
+        cssProcessor: require('cssnano'),
+        cssProcessorPluginOptions: {
+          preset: ['default', { discardComments: { removeAll: true } }],
+        },
+        canPrint: true
+      })
+    ],
+  }
 };

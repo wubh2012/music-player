1c91bbb34db7c9926bb2739c711ffd5e8f374f03
wubh2012 <wubh2012@163.com>
2019-08-12 23:15:33 +0800

    使用babel将es6代码转成es5

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,17 @@ module.exports = env => {
               outputPath: 'songs/'
             }
           }]
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime']
+            }
+          }
         }
       ]
     },
